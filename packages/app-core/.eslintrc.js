a6712799c184c3d129490100223cf6cb136d56e2
Rey <andrej.kolic@gmail.com>
2023-10-10 12:31:43 +0200

    fix eslint import issue

--- packages/app-core/.eslintrc.js
+++ packages/app-core/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ["custom/react-internal"],
   ignorePatterns: ["node_modules/", "dist/", "build/", "turbo/"],
+  rules: {
+    "import/no-extraneous-dependencies": [
+        "error",
+        { "devDependencies": true, "optionalDependencies": false, "peerDependencies": false }
+    ],
+  },
 };
--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -5,16 +5,10 @@ console.log('*', path.join(__dirname, './src'));
 module.exports = {
   extends: ["custom/react-internal"],
   ignorePatterns: ["node_modules/", "dist/", "build/", "turbo/"],
-  // plugins: ["import"],
-  settings: {
-    'import/resolver': {
-      alias: {
-        // map: [
-        //   ['~', './src'],
-        // ],
-        '~': path.join(__dirname, './src'),
-        extensions: ['.ts', 'tsx', '.js', '.jsx', '.json']
-      }
-    }
-  }
-};
+  rules: {
+    "import/no-extraneous-dependencies": [
+        "error",
+        { "devDependencies": true, "optionalDependencies": false, "peerDependencies": false }
+    ],
+  },
+}
