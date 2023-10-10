65cbf70006d327b0775f05d407e67457721b6c92
Rey <andrej.kolic@gmail.com>
2023-10-10 12:17:03 +0200

    rename project

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,4 +1,20 @@
+const path = require('path');
+
+console.log('*', path.join(__dirname, './src'));
+
 module.exports = {
   extends: ["custom/react-internal"],
   ignorePatterns: ["node_modules/", "dist/", "build/", "turbo/"],
+  // plugins: ["import"],
+  settings: {
+    'import/resolver': {
+      alias: {
+        // map: [
+        //   ['~', './src'],
+        // ],
+        '~': path.join(__dirname, './src'),
+        extensions: ['.ts', 'tsx', '.js', '.jsx', '.json']
+      }
+    }
+  }
 };
