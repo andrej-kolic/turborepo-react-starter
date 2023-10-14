0425ee12496ba7e3010c62f08f234d7f057c85ea
Rey <andrej.kolic@gmail.com>
2023-10-14 14:28:59 +0200

    add jest to commons package

---
+++ packages/commons/src/__test__/index.test.ts
@@ -0,0 +1,7 @@
+import { toUppercase } from "..";
+
+describe("Commons", () => {
+  it("should return uppercase string", () => {
+    expect(toUppercase("Hello wORld")).toBe("HELLO WORLD");
+  });
+});
