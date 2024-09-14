317b177776ab2aaf6cb0cb3431e495e1cb4191ea
Rey <andrej.kolic@gmail.com>
2024-09-14 23:58:27 +0200

    use syncpack for depencency validation

---
+++ .syncpackrc.cjs
@@ -0,0 +1,46 @@
+// @ts-check
+
+/** @type {import("syncpack").RcFile} */
+const config = {
+  dependencyTypes: ["prod", "dev"],
+
+  semverGroups: [
+    {
+      label: "use exact version numbers in production",
+      range: "",
+      dependencyTypes: [
+        "prod",
+        "resolutions",
+        "overrides",
+        "pnpmOverrides",
+        "peer",
+      ],
+      dependencies: ["**"],
+      packages: ["**"],
+    },
+    {
+      label: "use ^ version range for dev dependencies",
+      range: "^",
+      dependencyTypes: ["dev"],
+      dependencies: ["**"],
+      packages: ["**"],
+    },
+  ],
+
+  versionGroups: [
+    {
+      label: "@types packages should only be under devDependencies",
+      dependencies: ["@types/**"],
+      dependencyTypes: ["!dev"],
+      isBanned: true,
+    },
+    {
+      label: "Use workspace protocol when developing local packages",
+      dependencies: ["$LOCAL"],
+      dependencyTypes: ["dev"],
+      pinVersion: "workspace:*",
+    },
+  ],
+};
+
+module.exports = config;
