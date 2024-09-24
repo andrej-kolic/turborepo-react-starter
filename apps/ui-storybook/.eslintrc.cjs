3f5c95c3104e8ce2947a9bc1e86e7660e8df3bd3
Rey <andrej.kolic@gmail.com>
2024-09-24 23:45:55 +0200

    add lint and test:type scripts to ui-storybook

--- apps/ui-storybook/.eslintrc.cjs
+++ apps/ui-storybook/.eslintrc.cjs
@@ -4,17 +4,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: [
+    ".eslintrc.cjs",
+    "node_modules/",
+    "dist/",
+    "build/",
+    "turbo/",
+    "storybook-static",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
