8b11b9a0266bdcf57d3f84be7a6e01639d791919
Rey <andrej.kolic@gmail.com>
2024-10-02 21:38:38 +0200

    cleanup vite project

--- apps/app-vite/src/main.tsx
+++ apps/app-vite/src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from '~/App.tsx'
 import { AppCore } from "@repo/app-core";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <App /> */}
     <AppCore title="app-vite" href="#">
       <div>trt</div>
     </AppCore>
