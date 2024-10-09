b0d7a35478b22702c49cceec14f11ac331e8c9de
Rey <andrej.kolic@gmail.com>
2024-10-10 00:54:15 +0200

    extract netlify code to dedicated package

--- infra/netlify/tools/deploy-netlify.sh
+++ infra/netlify/tools/deploy-netlify.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# TODO: add netlify-cli to dev dependencies in proper package
-pnpm \
---package=netlify-cli dlx netlify deploy \
+pnpm netlify deploy \
 --dir apps/app-vite/dist \
 --site ${NETLIFY_SITE_ID} \
 --auth ${NETLIFY_AUTH_TOKEN} \
