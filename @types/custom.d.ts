6ac3abbc464d9920a07a84a2f1c58659ad703ff6
Rey <andrej.kolic@gmail.com>
2024-10-10 22:30:15 +0200

    add environment support in vite

--- @types/custom.d.ts
+++ @types/custom.d.ts
@@ -5,40 +5,41 @@
  *
  * See: https://webpack.js.org/guides/typescript/#importing-other-assets
  *      https://github.com/mrmckeb/typescript-plugin-css-modules#custom-definitions
+ *
+ * TODO: check vite/client types
  */
 
-declare module '*.module.css' {
-    const selectors: Record<string, string>;
-    export default selectors;
-  }
-  
-  declare module '*.css' {
-    const content: string;
-    export default content;
-  }
-  
-  // TODO: fix relative path
-  // NOTE: asset JavaScript files have to be referenced with an additional specifier end such as '?'
-  // declare module './assets/*' {
-  //   const reference: string;
-  //   export default reference;
-  // }
-  
-  // NOTE: asset JavaScript files have to be referenced with an additional specifier end such as '?'
-  declare module '~/assets/*' {
-    const reference: string;
-    export default reference;
-  }
-  
-  declare module '*.png' {
-    const reference: string;
-    export default reference;
-  }
-  
-  declare module '*.svg' {
-    const reference: string;
-  
-    // _eslint-disable-next-line import/no-default-export
-    export default reference;
-  }
-  
+declare module "*.module.css" {
+  const selectors: Record<string, string>;
+  export default selectors;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+// TODO: fix relative path
+// NOTE: asset JavaScript files have to be referenced with an additional specifier end such as '?'
+// declare module './assets/*' {
+//   const reference: string;
+//   export default reference;
+// }
+
+// NOTE: asset JavaScript files have to be referenced with an additional specifier end such as '?'
+declare module "~/assets/*" {
+  const reference: string;
+  export default reference;
+}
+
+declare module "*.png" {
+  const reference: string;
+  export default reference;
+}
+
+declare module "*.svg" {
+  const reference: string;
+
+  // _eslint-disable-next-line import/no-default-export
+  export default reference;
+}
--- apps/app-vite/vite.config.ts
+++ apps/app-vite/vite.config.ts
@@ -3,14 +3,48 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths"; // set up aliases from tsconfig
 
+// import dotenvx from '@dotenvx/dotenvx';
+
 // TODO: move to common? it's also used by webpack
 const __dirname = process.cwd();
 const pathResolve = (pathEntry) => path.resolve(__dirname, pathEntry);
 const appCorePublic = pathResolve("./node_modules/@repo/app-core/public");
+// const appCoreEnv = pathResolve("./node_modules/@repo/app-core/.env");
+const appCoreEnvDir = pathResolve("./node_modules/@repo/app-core/");
+
+// const de = dotenvx.config({
+//   path: appCoreEnv
+// });
+
+// // const de = dotenvx.config();
+// console.log('*** dotenvx:', de);
+
+// const dotEnvMap = new Map<string, unknown>();
+// for (const [key, value] of Object.entries(de.parsed)) {
+//   console.log(`Key: ${key}, Value: ${value}`);
+//   /**
+//    * add logic to filter variables, for ex. only if prefixed with REACT_APP prefix.
+//    * also add variables from process.env if necessary
+//    */
+//   dotEnvMap.set(`process.env.${key}`, JSON.stringify(value));
+// }
+
+// console.log('* map:', Object.fromEntries(dotEnvMap));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: appCoreEnvDir,
+  envPrefix: 'APP_REACT',
+
+  // define: Object.fromEntries(dotEnvMap),
+
+  // define: {
+  //   'process.env.API_URL': JSON.stringify(process.env.API_URL),
+  //   'process.env.TEST': JSON.stringify('abc'),
+  // },
+
   plugins: [react(), tsconfigPaths()],
+
   // resolve: {
   // preserveSymlinks: true,
   // alias: {
@@ -22,11 +56,11 @@ export default defineConfig({
   publicDir: appCorePublic,
 
   // TODO: remove, added for readable output
-  build: {
-    minify: false,
-    rollupOptions: {
-      // external: ["react", "react-dom", "react/jsx-runtime"],
-      treeshake: false,
-    },
-  },
+  // build: {
+  //   minify: false,
+  //   rollupOptions: {
+  //     // external: ["react", "react-dom", "react/jsx-runtime"],
+  //     treeshake: false,
+  //   },
+  // },
 });
