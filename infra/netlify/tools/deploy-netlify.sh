6668c7fdf8a22b59af490808984c5f812bf14d37
Rey <andrej.kolic@gmail.com>
2024-12-04 17:48:10 +0100

    deploy to netlify: env variables for local deploy

--- infra/netlify/tools/deploy-netlify.sh
+++ infra/netlify/tools/deploy-netlify.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+prod_flag=""
+if [ ${PRODUCTION} = "true" ]; then prod_flag="--prod"; fi
+
 pnpm netlify deploy \
---dir apps/app-vite/dist \
+--dir apps/${BUNDLER}/dist \
 --site ${NETLIFY_SITE_ID} \
 --auth ${NETLIFY_AUTH_TOKEN} \
---filter app-vite
+$prod_flag \
+--filter ${BUNDLER}
