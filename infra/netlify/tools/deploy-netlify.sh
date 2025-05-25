6961cc720782276d14fceda83b4e6f1f3e0a4f87
Rey <andrej.kolic@gmail.com>
2025-05-25 09:31:10 +0200

    update netflix deploy scripts

--- infra/netlify/tools/deploy-netlify.sh
+++ infra/netlify/tools/deploy-netlify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 prod_flag=""
-if [ ${PRODUCTION} = "true" ]; then prod_flag="--prod"; fi
+if [ ${BUILDENV} = "production" ]; then prod_flag="--prod"; fi
 
 pnpm netlify deploy \
 --dir apps/${BUNDLER}/dist \
 --site ${NETLIFY_SITE_ID} \
 --auth ${NETLIFY_AUTH_TOKEN} \
+--filter ${BUNDLER} \
 $prod_flag \
---filter ${BUNDLER}
+--json
