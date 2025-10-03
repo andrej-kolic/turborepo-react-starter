352d260d420083f2a341c368868ad467958bfca7
Rey <andrej.kolic@gmail.com>
2025-10-03 22:30:38 +0200

    update coments and logs

--- infra/netlify/tools/deploy-netlify.sh
+++ infra/netlify/tools/deploy-netlify.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Deploying to Netlify..."
-echo " "
-echo "BUNDLER: $BUNDLER"
-echo "BUILD_ENVIRONMENT: $BUILD_ENVIRONMENT"
-echo "NETLIFY_SITE_ID: $NETLIFY_SITE_ID"
-echo "NETLIFY_AUTH_TOKEN: $NETLIFY_AUTH_TOKEN"
-echo " "
+
+# echo " "
+# echo "BUNDLER: $BUNDLER"
+# echo "BUILD_ENVIRONMENT: $BUILD_ENVIRONMENT"
+# echo "NETLIFY_SITE_ID: $NETLIFY_SITE_ID"
+# echo " "
 
 set -e
 
