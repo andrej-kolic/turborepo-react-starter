1d66354d31f98dcebe380f651e911b7617628358
Rey <andrej.kolic@gmail.com>
2025-10-04 14:54:41 +0200

    update dependencies and migrate storybook

--- apps/ui-storybook/.storybook/main.ts
+++ apps/ui-storybook/.storybook/main.ts
@@ -1,16 +1,19 @@
+import { createRequire } from 'node:module';
+import { dirname, join } from 'node:path';
 import type { StorybookConfig } from '@storybook/react-vite';
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-docs'),
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
 
@@ -22,3 +25,7 @@ const config: StorybookConfig = {
   // },
 };
 export default config;
+
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
--- apps/ui-storybook/eslint.config.js
+++ apps/ui-storybook/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import {
   config as baseConfig,
   disableTypeCheck,
@@ -6,10 +9,9 @@ import {
 /** @type {import("eslint").Linter.Config} */
 export default [
   ...baseConfig,
-
   disableTypeCheck(['.storybook/*']),
-
   {
     ignores: ['storybook-static/'],
   },
+  ...storybook.configs['flat/recommended'],
 ];
