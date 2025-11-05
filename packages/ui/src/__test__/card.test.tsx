028fcc98ad6860c2c0a8a94ae5712d1ef11f879f
Rey <andrej.kolic@gmail.com>
2025-11-05 14:25:50 +0100

    fix test

--- packages/ui/src/__test__/card.test.tsx
+++ packages/ui/src/__test__/card.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Card } from '@repo/ui/card';
+import { Card } from '../card';
 
 describe('Card', () => {
   it('should render', () => {
